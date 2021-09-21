59377a4610c4da78029ea1a267251230cf14da3f
Fred-Olav <fred-olav@mosdal.org>
2021-09-21 05:20:37 +0200

    Lagt til induestrielle nettverk

---
+++ dignet/labels.pl
@@ -0,0 +1,21 @@
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# Associate labels original text with physical files.
+
+
+$key = q/diff_signaling_noise/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2020.2 (Released July 1, 2020)
+# labels from external_latex_labels array.
+
+
+$key = q/_/;
+$external_latex_labels{$key} = q|<|; 
+$noresave{$key} = "$nosave";
+
+1;
+
